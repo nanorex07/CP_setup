58d005ea8ecc06b993e7e79006f69519b9551b49
Namah Jain <33959345+nanorex07@users.noreply.github.com>
2021-11-12 10:22:20 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,15 +4,16 @@
 
 args=("$@")
 
+set -e
 
 if test -f "${args[0]}"; then
 
     if test -f "${args[1]}"; then
-        g++ ${args[0]}
+        g++ -std=c++17 ${args[0]}
         echo "[+] file compiled successfully"
         ./a.out < ${args[1]}
     else
-        g++ ${args[0]}
+        g++ -std=c++17 ${args[0]}
         echo "[+] file compiled successfully, running"
         ./a.out         
     fi
